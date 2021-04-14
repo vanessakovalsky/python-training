7439288522cadd026f0a90913ecd4ed9f034ba7a
Vanessa Kovalsky <vanessa.kovalsky@free.fr>
2021-04-14 12:32:50 +0200

    Fix vagrant file

--- resources/install.sh
+++ resources/install.sh
@@ -3,4 +3,7 @@
 apt-get update
 
 # install python
-apt-get install python-dev python-pip -q -y
+apt-get install python3 python3-dev python3-pip -q -y
+apt remove -y python 
+
+mv /usr/bin/python3 /usr/bin/python
