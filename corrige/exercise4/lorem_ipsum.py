2aecc89e72dc69a0aadfa60a2c62e11712e217ed
Vanessa Kovalsky <vanessa.kovalsky@free.fr>
2021-04-15 14:36:42 +0200

    Fix typo

--- corrige/exercise4/lorem_ipsum.py
+++ corrige/exercise4/lorem_ipsum.py
@@ -1,22 +1,18 @@
 # Lire le fichier loremipsum.txt (se trouvant dans le dossier resources)
 
-fichier = open("loremipsum.txt",,"r")
+fichier = open("loremipsum.txt","r+")
 
 #  Afficher le contenu du fichier depuis le script
 print(fichier.read())
 
-fichier.close()
 # Ajouter a la ligne 5 la chaine suivante (sur une nouvelle ligne) "Ma nouvelle chaine insérée dans mon fichier texte au milieu du lorem ipsum"
 
-fichier = open("loremipsum.txt","r")
-
+fichier.seek(0) # On remet le pointeur au début du fichier
 lines = fichier.readlines()
-fichier.close()
 
 lines[4] = lines[4] + "\nMa nouvelle chaine insérée dans mon fichier texte au milieu du lorem ipsum\n"
 
-fichier = open("loremipsum.txt","w")
-
+fichier.seek(0) # On remet le pointeur au début du fichier
 fichier.writelines(lines)
 
 fichier.close()
