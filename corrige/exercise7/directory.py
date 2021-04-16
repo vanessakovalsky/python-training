8b01ce74e4a37019dfc1081cbc2a637ae1bd42da
Vanessa Kovalsky <vanessa.kovalsky@free.fr>
2021-04-16 13:59:48 +0200

    Add correction for exercise 7

---
+++ corrige/exercise7/directory.py
@@ -0,0 +1,13 @@
+import os
+
+# Dans quel répertoire je suis ?
+print('pwd')
+print(os.getcwd())
+
+# Que contient le répertoire ?
+print('ls')
+print(os.listdir())
+
+# Nombre de fichier : 
+print('nombre de fichiers')
+print (len([name for name in os.listdir('.') if os.path.isfile(name)]))
---
+++ corrige/exercise7/main.py
@@ -0,0 +1,10 @@
+import mini_maxi
+import pair_impair
+
+valeur1 = input('Valeur 1 :')
+valeur2 = input('Valeur 2 :')
+
+print('Valeur la plus petite : ')
+print(mini_maxi.mini(valeur1,valeur2))
+print('Valeur la plus grande :')
+print(mini_maxi.maxi(valeur1,valeur2))
