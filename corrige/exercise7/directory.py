12d139ebc5f56e94153c983671913a10c793ae46
Vanessa Kovalsky <vanessa.kovalsky@free.fr>
2021-04-16 14:01:09 +0200

    Fix typo

--- corrige/exercise7/directory.py
+++ corrige/exercise7/directory.py
@@ -10,4 +10,4 @@ print(os.listdir())
 
 # Nombre de fichier : 
 print('nombre de fichiers')
-print (len([name for name in os.listdir('.') if os.path.isfile(name)]))
+print(len([name for name in os.listdir('.') if os.path.isfile(name)]))
