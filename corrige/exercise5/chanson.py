79986acf688e34f73045d827d65a260d1d5d797c
Vanessa Kovalsky <vanessa.kovalsky@free.fr>
2021-04-16 10:02:51 +0200

    Add missing test

--- corrige/exercise5/chanson.py
+++ corrige/exercise5/chanson.py
@@ -1,9 +1,11 @@
 # Répeter les paroles en adaptant les nombres
 
 n=10
-print("C'est dans % dans je m'en irai j'entends le loup le renard chanter" %n)
 
 while 0 < n < 11 :
+    if n > 1 : 
+        print("C'est dans % d ans je m'en irai j'entends le loup le renard chanter" %n)
+    else: 
+        print("C'est dans % d an je m'en irai j'entends le loup le renard chanter" %n)
 
-    print("C'est dans % d ans je m'en irai j'entends le loup le renard chanter" %n)
     n = n - 1
