90775cfabe88336b49719d13bbc4e63cb1cdefa1
vanessakovalsky <vanessa.kovalsky@free.fr>
2022-03-18 16:04:51 +0100

    Add module example

--- corrige/exercise4/lorem_ipsum.py
+++ corrige/exercise4/lorem_ipsum.py
@@ -1,6 +1,6 @@
 # Lire le fichier loremipsum.txt (se trouvant dans le dossier resources)
 
-fichier = open("loremipsum.txt","r+")
+fichier = open("../../resources/loremipsum.txt","r+")
 
 #  Afficher le contenu du fichier depuis le script
 print(fichier.read())
@@ -8,10 +8,14 @@ print(fichier.read())
 # Ajouter a la ligne 5 la chaine suivante (sur une nouvelle ligne) "Ma nouvelle chaine insérée dans mon fichier texte au milieu du lorem ipsum"
 
 fichier.seek(0) # On remet le pointeur au début du fichier
-lines = fichier.readlines()
+lines = fichier.readlines() # Liste des lignes
+
+print(lines)
 
 lines[4] = lines[4] + "\nMa nouvelle chaine insérée dans mon fichier texte au milieu du lorem ipsum\n"
 # Ou bien : lines.insert(4, "\nMa nouvelle chaine insérée dans mon fichier texte au milieu du lorem ipsum\n")
+
+print(lines)
 
 fichier.seek(0) # On remet le pointeur au début du fichier
 fichier.writelines(lines)
---
+++ exemples/module.py
@@ -0,0 +1,11 @@
+import portee_operation
+
+import datetime
+import math
+
+print(portee_operation.variable_demo)
+print(portee_operation.maSuperFonction('gklfdgjlksfd'))
+
+print(datetime.datetime.today())
+
+print(math.sqrt(121245))
