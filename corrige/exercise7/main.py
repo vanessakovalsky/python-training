20fa397f67145cc556749b9afc6407b3464fa147
vanessakovalsky <vanessa.kovalsky@free.fr>
2022-03-21 10:05:22 +0100

    Fix correction exercice 7

--- corrige/exercise7/main.py
+++ corrige/exercise7/main.py
@@ -1,10 +1,22 @@
 import mini_maxi
 import pair_impair
 
+type_donne = input('Pour entrer un texte taper 0, pour entrer un nombre taper 1')
+
 valeur1 = input('Valeur 1 :')
 valeur2 = input('Valeur 2 :')
 
-print('Valeur la plus petite : ')
-print(mini_maxi.mini(valeur1,valeur2))
-print('Valeur la plus grande :')
-print(mini_maxi.maxi(valeur1,valeur2))
+if type_donne == "0": 
+    mini = mini_maxi.mini(valeur1,valeur2)
+    maxi = mini_maxi.maxi(valeur1,valeur2)
+    print('Valeur la plus petite : ')
+    print(mini)
+    print('Valeur la plus grande :')
+    print(maxi)
+elif type_donne == "1":
+    pair = pair_impair.pair(valeur1)
+    impair = pair_impair.impair(valeur2)
+    print('La valeur 1 est pair :', pair)
+    print('La valeur 2 est impair :', impair)
+else:
+    print("Erreur de saisie")
