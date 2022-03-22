37e3e30d89d42a21da9979540477def0ca277fad
vanessakovalsky <vanessa.kovalsky@free.fr>
2022-03-22 09:54:59 +0100

    Correction exercice 9

--- corrige/exercise9/jeu.py
+++ corrige/exercise9/jeu.py
@@ -4,19 +4,47 @@ class Jeu():
     editeur = ""
     annee_de_sortie = ""
     descriptif = ""
-    categorie  = 'Familiale'
+    categorie  = ''
     duree = ''
     nombre_de_joueurs = ''
-    toto = ''
+
+    def __init__(self):
+        self.categorie = 'Familiale'
 
     def listeJeu(self):
-        pass
+        fichier = open('./corrige/exercise9/jeux.txt','r')
+        liste_jeux = fichier.readlines()
+        fichier.close()
+        return liste_jeux
 
-    def ajoutJeu(self):
-        pass
+    def ajoutJeu(self, nom_du_jeu, editeur, annee_de_sortie):
+        jeu = '\n' + nom_du_jeu + ',' + editeur + ',' + annee_de_sortie
+        fichier = open('./corrige/exercise9/jeux.txt','a')
+        fichier.write(jeu)
+        fichier.close()
+        return jeu + 'Le jeu a bien été ajouté'
 
     def voirJeu(self, identifiant_du_jeu):
-        pass
+        jeu_a_afficher = self.trouverunjeu(identifiant_du_jeu)
+        return jeu_a_afficher
 
     def supprimerJeu(self, identifiant_du_jeu):
-        pass
+        jeu_a_supprimer = self.trouverunjeu(identifiant_du_jeu)
+        fichier = open('./corrige/exercise9/jeux.txt','r')
+        liste_jeux = fichier.readlines()
+        fichier.close()
+        for jeu in liste_jeux:
+            if jeu_a_supprimer ==  jeu:
+                liste_jeux.remove(jeu_a_supprimer)
+        fichier = open('./corrige/exercise9/jeux.txt','w')
+        fichier.writelines(liste_jeux)
+        fichier.close()
+
+    def trouverunjeu(self,id):
+        liste_jeux = self.listeJeu()
+        jeu_a_afficher = ''
+        for jeu in liste_jeux:
+            jeu_split = jeu.split(',')
+            if jeu_split[0] == id:
+                jeu_a_afficher = jeu_split
+        return jeu_a_afficher
--- corrige/exercise9/main.py
+++ corrige/exercise9/main.py
@@ -1,4 +1,4 @@
-import jeu
+from jeu import Jeu
 
 
 jeu = Jeu()
@@ -6,12 +6,16 @@ jeu = Jeu()
 choix = input('Choisir 1 - Liste, 2 - Ajout, 3 - Voir un jeu, 4 - Supprimer')
 
 if choix == "1":
-    jeu.listeJeu()
+    for un_jeu in jeu.listeJeu():
+        print(un_jeu)
 elif choix == "2":
-    jeu.ajoutJeu()
+    nom_du_jeu = input('Nom du jeu')
+    editeur = input('Editeur du jeu')
+    annee_de_sortie = input('Annee de sortie')
+    print(jeu.ajoutJeu(nom_du_jeu, editeur, annee_de_sortie))
 elif choix == "3":
     identifiant = input('Nom du jeu')
-    jeu.voirJeu(identifiant)
+    print(jeu.voirJeu(identifiant))
 elif choix == "4":
     identifiant = input('Nom du jeu')
     jeu.supprimerJeu(identifiant)
